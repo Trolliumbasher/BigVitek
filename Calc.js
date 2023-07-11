f673c07fe63b81448fe37f0b3417fb29c2d612d2
Trolliumbasher <84348169+Trolliumbasher@users.noreply.github.com>
2023-07-11 22:08:02 +0300

    Create Calc.js

---
+++ Calc.js
@@ -0,0 +1,18 @@
+let num1 = prompt('первое число');
+num1 = Number(num1);
+
+let num2 = prompt('второе число');
+num2 = Number(num2);
+
+switch (operator) {
+    case '+':
+        alert (num1 + num2);
+        break;
+    case '-':
+        alert (num1 - num2);
+        break;
+    case '/':
+        alert (num1 / num2);
+        break;
+    case '*':
+        alert (num1 * num2);
